769204f00f458879911f883a587a5c98c9ec1910
David Shadix <david_a_shadix@homedepot.com>
2023-08-07 16:46:43 -0400

    update logs

--- artifactory/openmetrics.go
+++ artifactory/openmetrics.go
@@ -7,11 +7,11 @@ import (
 const openMetricsEndpoint = "v1/metrics"
 
 type OpenMetrics struct {
-	Metric string
-	NodeId string
+	PromMetrics string
+	NodeId      string
 }
 
-// FetchReplications makes the API call to replication endpoint and returns []Replication
+// FetchOpenMetrics makes the API call to open metrics endpoint and returns all the open metrics
 func (c *Client) FetchOpenMetrics() (OpenMetrics, error) {
 	var openMetrics OpenMetrics
 	level.Debug(c.logger).Log("msg", "Fetching openMetrics")
@@ -22,9 +22,11 @@ func (c *Client) FetchOpenMetrics() (OpenMetrics, error) {
 		}
 		return openMetrics, err
 	}
-	openMetrics.NodeId = resp.NodeId
-	openMetrics.Metric = string(resp.Body)
+
 	level.Debug(c.logger).Log("msg", "OpenMetrics from Artifactory", "body", string(resp.Body))
+
+	openMetrics.NodeId = resp.NodeId
+	openMetrics.PromMetrics = string(resp.Body)
 
 	return openMetrics, nil
 }
